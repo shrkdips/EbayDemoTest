396eb12bf4e32f6f159867c828fdb81ec2755d09
user <shreyaksambitpatnaik@gmail.com>
2020-03-31 17:15:06 +0530

    The First Commit of Old Eclipse Code

---
+++ EbayTest/src/test/java/com/HomePage/test/HomePageTest.java
@@ -0,0 +1,20 @@
+package com.HomePage.test;
+
+import org.testng.annotations.Test;
+
+public class HomePageTest
+{
+	
+	@Test
+	public void home()
+	{
+		System.out.println("Home Page Test Pass");
+	}
+	
+	@Test
+	public void homeNew()
+	{
+		System.out.println("New Home Page Test Also Pass");
+	}
+
+}
---
+++ EbayTest/src/test/java/com/LoginPage/test/LoginPageTest.java
@@ -0,0 +1,20 @@
+package com.LoginPage.test;
+
+import org.testng.annotations.Test;
+
+public class LoginPageTest
+{
+	
+	@Test
+	public void page()
+	{
+		System.out.println("Login Page Test Success");
+	}
+	
+	@Test
+	public void pageNew()
+	{
+		System.out.println("New Lopgin Page Test Success");
+	}
+
+}
